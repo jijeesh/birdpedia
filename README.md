4f0d0e4e2f49e07fd31ed455882d4deff3c0ec8a
Jijeesh <jijeesh.ka@gmail.com>
2019-05-24 16:14:39 +0530

    make the hello world app and test

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # birdpedia
+
+
+To run the server on your system:
+
+1. Make sure you have [dep](https://github.com/golang/dep) installed
+2. Run `dep ensure` to install dependencies
+3. Run `go build` to create the binary (`birdpedia`)
+4. Run the binary : `./birdpedia`
+
+To run tests:
+
+2. Run `dep ensure` to install dependencies
+2. Run `go test ./...`
---
+++ main.go
@@ -0,0 +1,40 @@
+// This is the name of our package
+package main
+
+// These are the libraries we are going to use
+
+import (
+	// Both fmt and net are part of the Go standard libraries
+	// The "net/http" library has methods to implement HTTP clients and servers
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	// Declare a new router
+	r := mux.NewRouter()
+	// The "HandleFunc" method accept a path and a function as arguments
+	// (Yes,  we can pass functions as arguments, and even trat them like variable in Go)
+	// However, the handler function has to have the appropriate signature (as discribed by the "handler" function below)
+	r.HandleFunc("/hello", handler).Methods("GET")
+	//r.HandleFunc("/hello", handler).Methods("GET")
+
+	// After defining our server, we finally "listen and serve" on port 8080
+	// The second argumetn is the handler , which we will come to later on,
+	// but for now it is left as nil,
+	// and the handler defined ablov (in "HandleFunc") is used
+	http.ListenAndServe(":8080", r)
+}
+
+/*
+"handler is our handler function. It has to follow the function signature of
+a ResponseWriter and Request type as the arguments
+"
+*/
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	// For this case, we will always pipe "Hello World" into the response wirter
+	fmt.Fprintf(w, "Hello World")
+}
